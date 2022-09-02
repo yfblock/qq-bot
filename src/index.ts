fbc3b301ca1479151c6ef33f0109bc9c00372d3c
yufeng <321353225@qq.com>
2022-09-02 18:09:29 +0800

    添加密码登录方式

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import { createClient } from "oicq"
 import  "./events"
 import { bot } from "./global"
 
+process.on("unhandledRejection", (reason, promise) => {
+	console.log('Unhandled Rejection at:', promise, 'reason:', reason)
+})
+
 bot.on("system.login.qrcode", function (_e) {
 	this.logger.mark("扫码后按Enter完成登录")
 	process.stdin.once("data", () => {
 		this.login()
 	})
-})
-
-process.on("unhandledRejection", (reason, promise) => {
-	console.log('Unhandled Rejection at:', promise, 'reason:', reason)
-})
-
-
-bot.login()
+}).login()
+// bot.on("system.login.slider", function(e) {
+// 	console.log("请输入ticket:");
+// 	process.stdin.once("data", ticket => this.submitSlider(String(ticket).trim()))
+// }).login(process.env.qq_password)
