063039158008b131c49d2026eac17ba0522cab1d
yufeng <321353225@qq.com>
2022-09-05 20:32:34 +0800

    更新验证方式

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,16 @@ process.on("unhandledRejection", (reason, promise) => {
 	console.log('Unhandled Rejection at:', promise, 'reason:', reason)
 })
 
-bot.on("system.login.qrcode", function (_e) {
-	this.logger.mark("扫码后按Enter完成登录")
-	process.stdin.once("data", () => {
-		this.login()
-	})
-}).login()
-// bot.on("system.login.slider", function(e) {
-// 	console.log("请输入ticket:");
-// 	process.stdin.once("data", ticket => this.submitSlider(String(ticket).trim()))
-// }).login(process.env.qq_password)
+if(process.env.qq_password) {
+	bot.on("system.login.slider", function(e) {
+		console.log("请输入ticket:");
+		process.stdin.once("data", ticket => this.submitSlider(String(ticket).trim()))
+	}).login(process.env.qq_password)
+} else {
+	bot.on("system.login.qrcode", function (_e) {
+		this.logger.mark("扫码后按Enter完成登录")
+		process.stdin.once("data", () => {
+			this.login()
+		})
+	}).login()
+}
